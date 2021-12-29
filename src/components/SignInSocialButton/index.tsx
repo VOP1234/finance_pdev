0123540c89a4ae4ce79380456224b3507131ce78
Vitor <vop1234@hotmail.com>
2021-12-29 16:28:52 -0300

    Creating Context

--- src/components/SignInSocialButton/index.tsx
+++ src/components/SignInSocialButton/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { RectButtonProps } from "react-native-gesture-handler";
 import { SvgProps } from "react-native-svg";
-import { Button, ImageContainer, Title } from "./styles";
+import { 
+  Button, 
+  ImageContainer, 
+  Title 
+} from "./styles";
 
 interface Props extends RectButtonProps {
   title: string;
